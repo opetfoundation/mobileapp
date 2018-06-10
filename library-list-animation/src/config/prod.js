d8e9cb219d961b7dd18fd10b76d3e40da352cde4
Ameya Kulkarni <ameya1295@gmail.com>
2018-06-11 01:17:16 +0530

    added config files

---
+++ library-list-animation/src/config/prod.js
@@ -0,0 +1,4 @@
+export const config = {
+    GraphcoolApiURI: "https://api.graph.cool/simple/v1/cj7revq6903sa0132ku7pl7r4",
+    GraphcoolFilesApiURI: "https://api.graph.cool/file/v1/cj7revq6903sa0132ku7pl7r4",
+};
